cd97cf53aacd6a1acdf287227c5cb1567d1d2f66
Kovas Palunas <kovas.palunas@gmail.com>
2021-03-13 19:01:51 -0800

    first commit, untested but should work in theory

---
+++ README.md
@@ -0,0 +1,16 @@
+aw-watcher-google-drive
+================
+
+Track a directory in Google Drive with spreadsheet data, and import it into
+ActivityWatch.
+
+# Build
+
+Make sure you have Python 3.7+ and `poetry` installed, then install with `poetry install`.
+
+
+# Usage
+
+Run `aw-watcher-input --help` for usage instructions.
+
+We might eventually create binary builds (like the ones bundled with ActivityWatch for aw-watcher-afk and aw-watcher-window) to make it easier to get this watcher up and running, but it's still a bit too early for that.
---
+++ src/aw_watcher_google_drive/main.py
@@ -0,0 +1,36 @@
+from time import sleep
+import logging
+
+import click
+
+import aw_client
+from aw_core import Event
+import drive_api
+
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option("--testing", is_flag=True)
+def main(testing: bool):
+    sleep(5)  # Without this the watcher will crash if it is started by default
+    logging.basicConfig(level=logging.INFO)
+    logger.info("Starting watcher...")
+    client = aw_client.ActivityWatchClient(
+        "aw-watcher-google-drive", testing=testing)
+
+    poll_time = 1
+
+    while True:
+        sleep(poll_time)
+
+        for fname, data in drive_api.read_all_spreadsheet_data("activitywatch"):
+            # Create bucket
+            bucket_name = fname
+            eventtype = "os.hid.input"
+            client.create_bucket(bucket_name, eventtype, queued=False)
+
+            # Create events
+            for line in data:
+                e = Event(timestamp=line['time'], duration=5, data=line)
+                client.heartbeat(bucket_name, e, queued=False, commit_interval=5)
